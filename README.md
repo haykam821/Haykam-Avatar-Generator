5744a52e6fcdeee91a1a79f9a646c9ba0654ffd6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-10 23:02:09 +0000

    Deploy haykam821/Haykam-Avatar-Generator to github.com/haykam821/Haykam-Avatar-Generator.git:gh-pages

--- README.md
+++
@@ -1,1 +0,0 @@
-# Haykam-Avatar-Generator
--- src/magic.js
+++
@@ -1,8 +0,0 @@
-const React = require("react");
-const styled = require("styled-components");
-
-module.exports = {
-	size: 300,
-	elem: React.createElement,
-	style: styled.default,
-};
