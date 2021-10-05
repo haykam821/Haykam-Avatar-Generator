dd33c05d5184c3a80c3214628b3c71e58def289f
haykam821 <24855774+haykam821@users.noreply.github.com>
2021-10-05 19:18:57 -0400

    Allow forcing exported avatar URLs to include options with default values

--- src/components/sharing-link.tsx
+++ src/components/sharing-link.tsx
@@ -5,12 +5,20 @@ import RawInput from "./raw-input";
 import React from "react";
 import styled from "styled-components";
 
+enum OptionFilter {
+	DEFAULTED,
+	FULL,
+}
+
+const defaultFilter: OptionFilter = OptionFilter.DEFAULTED;
+
 interface SharingLinkProps {
 	className?: string;
 	location: Location;
 	optionManager: OptionManager;
 }
 interface SharingLinkState {
+	filter: OptionFilter;
 	link: string;
 }
 
@@ -19,12 +27,14 @@ class SharingLinkUnstyled extends React.Component<SharingLinkProps, SharingLinkS
 		super(props);
 
 		this.state = {
-			link: this.getLink(),
+			filter: defaultFilter,
+			link: this.getLink(defaultFilter),
 		};
 
 		this.updateLink = this.updateLink.bind(this);
 		this.props.optionManager.addEventListener("update", this.updateLink);
 
+		this.invertFilter = this.invertFilter.bind(this);
 		this.attemptShare = this.attemptShare.bind(this);
 	}
 
@@ -32,14 +42,15 @@ class SharingLinkUnstyled extends React.Component<SharingLinkProps, SharingLinkS
 		this.props.optionManager.removeEventListener("update", this.updateLink);
 	}
 
-	private getLink(): string {
+	private getLink(filter: OptionFilter): string {
 		const url = new URL(this.props.location.href);
 
 		const options = Object.entries(this.props.optionManager.getAll());
 		for (const [key, value] of options) {
-			if (value !== undefined && !this.props.optionManager.isDefault(key)) {
-				url.searchParams.set(key, value + "");
-			}
+			if (value === undefined) continue;
+			if (filter === OptionFilter.DEFAULTED && this.props.optionManager.isDefault(key)) continue;
+
+			url.searchParams.set(key, value + "");
 		}
 
 		return url.toString();
@@ -47,7 +58,15 @@ class SharingLinkUnstyled extends React.Component<SharingLinkProps, SharingLinkS
 
 	private updateLink(): void {
 		this.setState({
-			link: this.getLink(),
+			link: this.getLink(this.state.filter),
+		});
+	}
+
+	private invertFilter(): void {
+		const filter = this.state.filter === OptionFilter.DEFAULTED ? OptionFilter.FULL : OptionFilter.DEFAULTED;
+		this.setState({
+			filter,
+			link: this.getLink(filter),
 		});
 	}
 
@@ -65,7 +84,7 @@ class SharingLinkUnstyled extends React.Component<SharingLinkProps, SharingLinkS
 	}
 
 	render(): JSX.Element {
-		return <div className={this.props.className}>
+		return <div className={this.props.className} onDoubleClick={this.invertFilter}>
 			<Paragraph text="Copy the following link to share the avatar with others:" />
 			<RawInput readOnly value={this.state.link} />
 			{this.canShare() && <Button label="Share" onClick={this.attemptShare} />}
